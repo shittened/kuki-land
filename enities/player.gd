0ac4c06aba90d8d39a1670f33568233c3fce0c4f
shitten <kiefciman@gmail.com>
2025-03-03 01:01:16 +0200

    a

--- enities/player.gd
+++ enities/player.gd
@@ -1,30 +1,46 @@
 extends CharacterBody2D
 
-var speed:int = 50
+var speed:int
+var normal_speed:int = 50
+var carrying_speed:int = floor(self.normal_speed / 2)
 var facing_direction:String = 'down'
 var direction:Vector2
 var axe_dmg:int = 30
 var equipped_tool:String = 'axe'
 var axe_distance:int = 60
+var pickup_distance:int = 30
 var luck:int = 10
 var force: int = 30000
+var inventory: Dictionary
+var carrying: bool = false
+var carried_object: String
 
 func Animations():
+	var prefix: String
+	
+	if self.carrying:
+		prefix = self.carried_object + '_'
+	else:
+		prefix = ''
+		
 	if direction.x > 0:
-		$AnimatedSprite2D.animation = 'walking_right'
+		$AnimatedSprite2D.animation = prefix + 'walking_right'
 		facing_direction = 'right'
 	elif direction.x < 0:
-		$AnimatedSprite2D.animation = 'walking_left'
+		$AnimatedSprite2D.animation = prefix + 'walking_left'
 		facing_direction = 'left'
 	else:
 		if direction.y > 0:
-			$AnimatedSprite2D.animation = 'walking_down'
+			$AnimatedSprite2D.animation = prefix + 'walking_down'
 			facing_direction = 'down'
 		elif direction.y < 0:
-			$AnimatedSprite2D.animation = 'walking_up'
+			$AnimatedSprite2D.animation = prefix + 'walking_up'
 			facing_direction = 'up'
 		else:
-			$AnimatedSprite2D.animation = 'idle_' + facing_direction
+			if self.carrying:
+				$AnimatedSprite2D.animation = self.carried_object + '_idle_' + facing_direction
+			else:
+				$AnimatedSprite2D.animation = 'idle_' + facing_direction
 
 func Movement():
 	direction = Input.get_vector("move_left", "move_right", "move_up", "move_down").limit_length()
@@ -63,10 +79,21 @@ func Interact():
 
 func PickUp():
 	for item in self.get_parent().get_node('items').get_children():
-		if $origin.global_position.distance_to(item.global_position) <= 20:
+		if Input.is_action_just_pressed("e"):
+			if not $origin.global_position.distance_to(item.global_position) <= pickup_distance:
+				return
+			if not item.hovered:
+				return
 			item.visible = false
 			item.queue_free()
-
+			if item.is_big and not self.carrying:
+				self.carrying = true
+				self.carried_object = item.type
+				self.speed = self.carrying_speed
+			else:
+				if self.inventory.get_or_add(item.type) == null:
+					self.inventory[item.type] = 0
+				self.inventory[item.type] += 1
 func Push():
 	if not self.move_and_slide():
 		return
@@ -81,10 +108,11 @@ func Push():
 func _ready():
 	$AnimatedSprite2D.animation = 'idle_down'
 	$AnimatedSprite2D.play()
+	self.speed = self.normal_speed
 
 func _physics_process(delta):
 	Animations()
 	Movement()
 	Interact()
-	#PickUp()
+	PickUp()
 	Push()
